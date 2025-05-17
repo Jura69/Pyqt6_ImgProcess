e8199b550607ecb8c95e4e03b50da85226b2f0b1
Truong Tuan Loc <loctruongtuan@gmail.com>
2025-05-17 17:06:15 +0700

    Add processor factory class

--- image_processors/processor_factory.py
+++ image_processors/processor_factory.py
@@ -2,8 +2,6 @@ from .processors.rotation_processor import RotationProcessor
 from .processors.crop_processor import CropProcessor
 
 class ProcessorFactory:
-    """Factory class for creating image processors"""
-    
     _processors = {
         "Rotation": RotationProcessor,
         "Crop": CropProcessor
@@ -12,13 +10,7 @@ class ProcessorFactory:
     @classmethod
     def get_processor(cls, name):
         return cls._processors.get(name)()
-    
+        
     @classmethod
     def get_available_processors(cls):
-        """Get list of available processor names"""
         return list(cls._processors.keys())
-    
-    @classmethod
-    def register_processor(cls, name: str, processor_class):
-        """Register a new processor type"""
-        cls._processors[name] = processor_class 
