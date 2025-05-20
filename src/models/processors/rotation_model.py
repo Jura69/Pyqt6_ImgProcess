fix rotation

src/models/processors/rotation_model.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math
+import cv2
 from typing import Tuple, Literal
 
 class RotationModel:
@@ -25,14 +26,9 @@ class RotationModel:
         return self.rotate(image, self.degree)
     
     def rotate(self, image: np.ndarray, degree: float) -> np.ndarray:
-        """Xoay ảnh theo góc (độ)."""
-        if image is None:
-            raise ValueError("Ảnh đầu vào không được None")
-        
-        # Đổi độ sang radian, âm là xoay thuận chiều kim đồng hồ
+        """Xoay ảnh theo góc (độ)"""
         theta = -degree * math.pi / 180
         height, width = image.shape[:2]
-        
         if self.rotation_type == "center":
             MT1 = np.float32([
                 [1, 0, width//2],
@@ -56,13 +52,8 @@ class RotationModel:
                 [math.cos(theta), -math.sin(theta), 0],
                 [math.sin(theta), math.cos(theta), 0]
             ])
-        rotated = np.zeros_like(image)
-        for y in range(height):
-            for x in range(width):
-                new_x = int(M[0,0] * x + M[0,1] * y + M[0,2])
-                new_y = int(M[1,0] * x + M[1,1] * y + M[1,2])
-                if 0 <= new_x < width and 0 <= new_y < height:
-                    rotated[new_y, new_x] = image[y, x]
+      
+        rotated = cv2.warpAffine(image, M, (width, height))
         return rotated
     
     def get_name(self) -> str: