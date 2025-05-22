132a6222fea6d85db2727e73372a2e3636312ff4
Truong Tuan Loc <loctruongtuan@gmail.com>
2025-05-22 15:59:37 +0700

    update scaling

--- src/utils/imageScaling_ultil.py
+++ src/utils/imageScaling_ultil.py
@@ -6,9 +6,8 @@ def image_scaling(image, max_width=650, max_height=650):
     scaleX = max_width / w
     scaleY = max_height / h
     scale = min(scaleX, scaleY)
-    if scale == 1:
-        return image  # Không cần scale
+    
     new_w = int(w * scale)
     new_h = int(h * scale)
     M = np.float32([[scale, 0, 0], [0, scale, 0]])
-    return cv2.warpAffine(image, M, (new_w, new_h), flags=cv2.INTER_LINEAR) 
+    return cv2.warpAffine(image, M, (new_w, new_h)) 
